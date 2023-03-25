22874615be6dd4bbf6e6f85d30c8c091e2bd6983
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-25 22:13:49 +0200

    Update solve.sh

--- Web/Custom request 2 (200)/solve.sh
+++ Web/Custom request 2 (200)/solve.sh
@@ -1,1 +1,3 @@
-curl '213.177.18.70:8008/second/flag?flag=please' -H 'Sibiu-Academic-CTF: 978f6f608df5279d4d85e700d83ac873'
+#!/bin.bash
+
+/bin/curl '213.177.18.70:8008/second/flag?flag=please' -H 'Sibiu-Academic-CTF: 978f6f608df5279d4d85e700d83ac873'
